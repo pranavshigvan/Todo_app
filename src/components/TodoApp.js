bcfe3169e50798f9e460a5a7255641bb00510e85
pranavshigvan <pranavshigvan005@gmail.com>
2022-12-22 20:56:36 +0530

    ...

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -2,7 +2,8 @@ import React from 'react'
 let style = {
     maxWidth:"450px",
     width:"100%",
-    border:"1px solid red"
+    border:"1px solid red",
+    margin:"0 auto"
 }
 function TodoApp(props) {
    return (
